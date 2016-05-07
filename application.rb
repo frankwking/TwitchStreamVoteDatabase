be05f56c53e90b27a6ad40f78d4ffd33d81848e4
Frank King <frankwking@gmail.com>
2016-05-07 19:54:16 -0400

    add reloader to main application

--- application.rb
+++ application.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require 'sinatra/reloader'
 require_relative 'db/models'
 require_relative 'app/controllers/init'
 require 'tilt/haml'
 
 class TwitchStreamVoteDatabase < Sinatra::Base
+  set :environment, :development
+  configure :development do
+    register Sinatra::Reloader
+  end
   register Sinatra::ActiveRecordExtension
   use Rack::MethodOverride
   set :database, adapter: 'sqlite3', database: 'data.sqlite3'
