35553fb9ac7f394718bac495425eba2e6b85c8a6
Frank King <frankwking@gmail.com>
2016-05-07 19:53:58 -0400

    added sinatra reloader to game queue

--- app/controllers/GameQueueController.rb
+++ app/controllers/GameQueueController.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class TwitchStreamVoteDatabase < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/game_queue/:id/?' do
     @gameQueue = GameQueue.find params[:id]
     haml :game_queue_show
