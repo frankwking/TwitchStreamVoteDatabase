afcae74535f28ec8a5cd9490309f6e3e53f8c4f1
Frank King <frankwing@users.norely.github.com>
2016-05-11 16:53:16 -0400

    temporatily disable game record

--- app/controllers/init.rb
+++ app/controllers/init.rb
@@ -1,4 +1,4 @@
 # require_relative 'GameQueueController'
-require_relative 'GameController'
+# require_relative 'GameController'
 require_relative 'GameConsoleController'
 require_relative 'GameGenreController'
