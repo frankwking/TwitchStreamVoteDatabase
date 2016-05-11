d3e924f37487ec1311fdf99f0dda09f7ab706bad
Frank King <frankwing@users.norely.github.com>
2016-05-10 21:11:59 -0400

    create vote record table

---
+++ db/migrate/20160511011109_create_vote_record_table.rb
@@ -0,0 +1,9 @@
+class CreateVoteRecordTable < ActiveRecord::Migration
+  def change
+    create_table :vote_records, do |t|
+      t.belongs_to :game, index: true
+      t.datetime :vote_record_date
+      t.timestamps null: false
+    end
+  end
+end
