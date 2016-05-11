5a5f9f08e851d95dca12ecc4b16d243034378792
Frank King <frankwing@users.norely.github.com>
2016-05-10 20:19:57 -0400

    Create new models.rb

--- db/models.rb
+++ db/models.rb
@@ -1,11 +1,25 @@
-# A Queue is a list of Games
-class GameQueue < ActiveRecord::Base
-  has_many :Games
-  validates_presence_of :name
+# A game console has many game and many games have the same console
+class GameConsole < ActiveRecord::Base
+    has_and_belongs_to_many :games
+    validates_presence_of :name
 end
 
-# A Game is a record of a videogame to be played
+# A game genre has many games and many games have the same genre
+class GameGenre < ActiveRecord::Base
+    has_and_belongs_to_many :games
+    validates_presence_of :name
+
+end
+
+# A vote record belongs to a single game, which will have many of them
+class VoteRecord < ActiveRecord::Base
+    belongs_to :game
+end
+
+# A game record is our most important object, it belongs to many genres and consoles and it has many vote records
 class Game < ActiveRecord::Base
-  has_and_belongs_to_many :game_queues
-  validates_presence_of :name
+    has_and_belongs_to_many :game_consoles
+    has_and_belongs_to_many :game_genres
+    has_many :vote_records
+    validates_presence_of :name
 end
