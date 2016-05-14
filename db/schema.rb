a34127e46c86d30aca3feace0e4bb0544f141f62
Frank King <frankwing@users.norely.github.com>
2016-05-14 13:53:36 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514173338) do
+ActiveRecord::Schema.define(version: 20160514175228) do
 
   create_table "game_consoles", force: :cascade do |t|
     t.string   "name"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20160514173338) do
   add_index "game_genres_games", ["game_genre_id"], name: "index_game_genres_games_on_game_genre_id"
   add_index "game_genres_games", ["game_id"], name: "index_game_genres_games_on_game_id"
 
-  create_table "game_queue", force: :cascade do |t|
+  create_table "game_queues", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
