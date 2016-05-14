8d1affd6522470ab5bfd8c7de6558116a053d9ad
Frank King <frankwing@users.norely.github.com>
2016-05-14 19:24:56 -0400

    added some queues and consoles to seeds

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# ruby encoding: utf-8
+GameQueue.create(name: 'Not in Game Queue')
+GameQueue.create(name: 'Shorter than 30 Hours')
+GameQueue.create(name: 'Longer than 30 Hours')
+GameConsole.create(name: 'SNES')
+GameConsole.create(name: 'Nintendo 64')
+GameConsole.create(name: 'Xbox 360')
