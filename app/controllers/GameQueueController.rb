b02314824c3f95958991c9c0515b002ee4874349
Frank King <frankwing@users.norely.github.com>
2016-05-14 19:24:38 -0400

    updated game queue controller, removed creation/deletion which we are not going to use

--- app/controllers/GameQueueController.rb
+++ app/controllers/GameQueueController.rb
@@ -6,30 +6,13 @@ class TwitchStreamVoteDatabase < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/game_queue/:id/?' do
+  get '/game_queues/:id/?' do
     @gameQueue = GameQueue.find params[:id]
     haml :game_queue_show
-  end
-
-  delete '/game_queue/:id/?' do
-    gameQueue = GameQueue.find params[:id]
-    gameQueue.destroy
-
-    redirect '/game_queues/'
   end
 
   get '/game_queues/?' do
     @gameQueue = GameQueue.all
     haml :game_queue_index
-  end
-
-  get '/new_game_queue/?' do
-    haml :new_game_queue
-  end
-
-  post '/new_game_queue/?' do
-    GameQueue.create(params)
-
-    redirect '/game_queues/'
   end
 end
