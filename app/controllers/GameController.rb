b9f8487e20cbdf9ce303f90d7fec227693e276a8
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-05-15 11:59:50 -0400

    Added genre functionality to GameController

--- app/controllers/GameController.rb
+++ app/controllers/GameController.rb
@@ -9,6 +9,7 @@ class TwitchStreamVoteDatabase < Sinatra::Base
   get '/games/:id/?' do
     @game = Game.find params[:id]
     @gameConsoles = GameConsole.all
+    @gameGenres = GameGenre.all
     @current_votes = @game.current_votes.to_s
     haml :game_show
   end
@@ -27,15 +28,18 @@ class TwitchStreamVoteDatabase < Sinatra::Base
 
   get '/new_game/?' do
     @gameConsoles = GameConsole.all
+    @gameGenres = GameGenre.all
     haml :new_game
   end
 
   post '/new_game/?' do
     game_console_id = params[:game_console_id]
     params.delete 'game_console_id'
+    game_genre_id = params[:game_genre_id]
+    params.delete 'game_genre_id'
     game = Game.create(params)
     game.game_consoles << GameConsole.find(game_console_id)
-
+    game.game_genres << GameGenre.find(game_genre_id)
     redirect '/games/'
   end
 
@@ -46,6 +50,16 @@ class TwitchStreamVoteDatabase < Sinatra::Base
 
   delete '/games/:id/change_console/?' do
     Game.find(params[:id]).game_consoles.destroy(GameConsole.find(params[:game_console_id]))
+    redirect '/games/' + params[:id] + '/'
+  end
+
+  post '/games/:id/change_genre/?' do
+    Game.find(params[:id]).game_genres << GameGenre.find(params[:game_genre_id])
+    redirect '/games/' + params[:id] + '/'
+  end
+
+  delete '/games/:id/change_genre/?' do
+    Game.find(params[:id]).game_genres.destroy(GameGenre.find(params[:game_genre_id]))
     redirect '/games/' + params[:id] + '/'
   end
 
