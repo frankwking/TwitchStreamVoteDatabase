031d95b20094df68f813160206957104e97257a2
Frank King <frankwking@gmail.com>
2016-05-16 13:58:19 -0400

    queue show page now gets passed the games in the queue delete game from queue now added

--- app/controllers/GameQueueController.rb
+++ app/controllers/GameQueueController.rb
@@ -8,11 +8,18 @@ class TwitchStreamVoteDatabase < Sinatra::Base
 
   get '/game_queues/:id/?' do
     @gameQueue = GameQueue.find params[:id]
+    @games = @gameQueue.games
     haml :game_queue_show
   end
 
   get '/game_queues/?' do
     @gameQueue = GameQueue.all
     haml :game_queue_index
+  end
+
+  delete '/game_queues/:id/change_game/?' do
+    GameQueue.find(params[:id]).games.destroy(Game.find(params[:game_id]))
+
+    redirect '/game_queues/' + params[:id] +'/'
   end
 end
