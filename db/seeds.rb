7c7adaa0e76ae0a353184a44ae011dcb28f85407
Frank King <frankwking@gmail.com>
2016-05-24 17:15:53 -0400

    added three sample games to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,32 @@
 # ruby encoding: utf-8
-GameQueue.create(name: 'Not in Game Queue', display: false)
-GameQueue.create(name: 'Shorter than 30 Hours', display: true)
-GameQueue.create(name: 'Longer than 30 Hours', display: true)
-GameConsole.create(name: 'SNES')
-GameConsole.create(name: 'Nintendo 64')
-GameConsole.create(name: 'Xbox 360')
-GameGenre.create(name: 'JRPG')
-GameGenre.create(name: 'RTS')
-GameGenre.create(name: 'Walking Simulator')
+notQueue =    GameQueue.create(name: 'Not in Game Queue', display: false)
+shortQueue =  GameQueue.create(name: 'Shorter than 30 Hours', display: true)
+longQueue =   GameQueue.create(name: 'Longer than 30 Hours', display: true)
+snesConsole = GameConsole.create(name: 'SNES')
+n64Console =  GameConsole.create(name: 'Nintendo 64')
+x360Console = GameConsole.create(name: 'Xbox 360')
+jrpgGenre =   GameGenre.create(name: 'JRPG')
+rtsGenre =    GameGenre.create(name: 'RTS')
+walkGenre =   GameGenre.create(name: 'Walking Simulator')
+platGenre =   GameGenre.create(name: 'Platformer')
+
+chronoTrigger = Game.create(name: 'Chrono Trigger')
+chronoTrigger.game_consoles << GameConsole.find(snesConsole.id)
+chronoTrigger.game_genres << GameGenre.find(jrpgGenre.id)
+chronoTrigger.game_queue = GameQueue.find(shortQueue.id)
+chronoTrigger.current_votes = 5
+chronoTrigger.save
+
+secretOfMana = Game.create(name: 'Secret of Mana')
+secretOfMana.game_consoles << GameConsole.find(snesConsole.id)
+secretOfMana.game_genres << GameGenre.find(jrpgGenre.id)
+secretOfMana.game_queue = GameQueue.find(shortQueue.id)
+secretOfMana.current_votes = 1
+secretOfMana.save
+
+superMarioWorld = Game.create(name: 'Super Mario World')
+superMarioWorld.game_consoles << GameConsole.find(snesConsole.id)
+superMarioWorld.game_genres << GameGenre.find(platGenre.id)
+superMarioWorld.game_queue = GameQueue.find(shortQueue.id)
+superMarioWorld.current_votes = 3
+superMarioWorld.save
