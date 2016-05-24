bca71e5a24854a39cad88329555f542bcd13b6fb
Frank King <frankwking@gmail.com>
2016-05-24 18:15:02 -0400

    require new models init file and sinatra to new views folder

--- application.rb
+++ application.rb
@@ -1,8 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
-require_relative 'db/models'
 require_relative 'app/controllers/init'
+require_relative 'app/models/init'
 require 'tilt/haml'
 
 class TwitchStreamVoteDatabase < Sinatra::Base
@@ -14,7 +14,7 @@ class TwitchStreamVoteDatabase < Sinatra::Base
   use Rack::MethodOverride
   set :database, adapter: 'sqlite3', database: 'data.sqlite3'
   set :server, 'thin'
-  set :views, File.dirname(__FILE__) + "/views"
+  set :views, File.dirname(__FILE__) + "/app/views"
 
   get '/' do
     haml :index
